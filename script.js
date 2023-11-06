b2246b78e2af3b495ef8c37426e6abfbc3b39c28
raniellyaffonso <147007407+raniellyaffonso@users.noreply.github.com>
2023-11-06 20:38:08 -0300

    script.js
    Adicionei comentarios ao codigo

--- script.js
+++ script.js
@@ -4,17 +4,17 @@ const incentivo = document.getElementById("incentivo");
 const listaPalavras = []
 const botao = document.getElementById("botao");
 
-botao.addEventListener("click", () => {
-  res.classList.remove("res");
-  let input = document.getElementById("entrada").value;
-  fetch(`${url}${input}`)
-    .then((Response) => Response.json())
-    .then((data) => {
-      if (Array.isArray(data) && data.length > 0) {
-        incentivo.innerHTML = `<h3 id="incentivo" class="incentivo">a palavra ${input} existe! mandou bem.</h3>`; 
+botao.addEventListener("click", () => { // clica no botão e faz isso
+  res.classList.remove("res"); // quando clicar no botão aparece toda a classe
+  let input = document.getElementById("entrada").value; // entrada (palavra digitada)
+  fetch(`${url}${input}`) // pesquisa a url junto com o input, verifica se a palavra existe
+    .then((Response) => Response.json()) //pega o arquivo em json
+    .then((data) => { //pega esse dado aqui
+      if (Array.isArray(data) && data.length > 0) { // se a lista que ele puxar for maior que 0, significa que existe
+        incentivo.innerHTML = `<h3 id="incentivo" class="incentivo">a palavra ${input} existe! mandou bem.</h3>`; //muda no html a frase "paabens"
         listaPalavras.push(" " + input);
-        console.log(listaPalavras);
-      } else {
+        console.log(listaPalavras); // criar uma lista de palavra. (palavras certas e erradas)
+      } else { 
         incentivo.innerHTML = `<h3 id="incentivo" class="incentivo">a palavra ${input} não existe! tente de novo.</h3>`;
       }
     });
