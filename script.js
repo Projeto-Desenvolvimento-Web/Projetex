2d2c0b631e5046745c93b372db12a85b52b81492
giomendescorrea <147007498+giomendescorrea@users.noreply.github.com>
2023-11-10 15:34:46 -0300

    erros corrigidos
    jogo tá pronto. todos os erros de NaN foram corrigidos.

--- script.js
+++ script.js
@@ -3,25 +3,22 @@ const url = "https://api.dictionaryapi.dev/api/v2/entries/en/";
 const incentivo = document.getElementById("incentivo");
 const certas = document.getElementById("certas");
 const erradas = document.getElementById("erradas");
-let score = parseInt(scoreElement.innerHTML);
-
+let scoreElement = document.getElementById("score");
+let score = parseInt(scoreElement.innerHTML) || 0;
 const listaCertas = [];
 const listaErradas = [];
 const botao = document.getElementById("botao");
-let tempo; // Declare the tempo variable
+let tempo;
 
 botao.addEventListener("click", () => {
   res.classList.remove("res");
   let input = document.getElementById("entrada").value;
 
-  // Clear previous timer before starting a new one
   clearInterval(tempo);
 
   fetch(`${url}${input}`)
     .then((Response) => Response.json())
     .then((data) => {
-      let score = parseInt(scoreElement.innerHTML); // Parse the current score from the HTML
-
       if (Array.isArray(data) && data.length > 0) {
         let palavraEncontrada = listaCertas.includes(input);
         if (palavraEncontrada) {
@@ -29,7 +26,7 @@ botao.addEventListener("click", () => {
         } else {
           incentivo.innerHTML = `<h3 id="incentivo" class="incentivo">A palavra ${input} existe! Mandou bem.</h3>`;
           listaCertas.push(input);
-          parseInt(score += 10); // Increment the score by 10
+          score += 10;
           scoreElement.innerHTML = `<h3 id="score">${score}</h3>`;
           certas.innerHTML = `<p id="certas" class="certas">${listaCertas.join(", ")}</p>`;
         }
@@ -40,14 +37,13 @@ botao.addEventListener("click", () => {
         } else {
           incentivo.innerHTML = `<h3 id="incentivo" class="incentivo">A palavra ${input} não existe! Tente de novo.</h3>`;
           listaErradas.push(input);
-          parseInt(score -= 2); // Decrement the score by 2
+          score -= 2;
           scoreElement.innerHTML = `<h3 id="score">${score}</h3>`;
           erradas.innerHTML = `<p id="erradas" class="erradas">${listaErradas.join(", ")}</p>`;
         }
       }
     });
 
-  // Start the countdown timer after fetching data
   contagem();
 });
 
@@ -60,7 +56,8 @@ document.getElementById("botao").addEventListener("click", function () {
 function contagem() {
   tempo = setInterval(function () {
     var cronometro = document.getElementById('tempo').innerHTML;
-    var soma = parseInt(cronometro) - 1;
+    var soma = parseInt(cronometro, 10);
+    soma = isNaN(soma) ? 0 : soma - 1;
     if (soma === 0) {
       document.getElementById('tempo').innerHTML = "Tempo esgotado";
       pararcontagem();
